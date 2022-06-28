4bf4772e65ea1049aa9b10574193aa5f2b5994f0
Shawn Freeman <magus_zsf@hotmail.com>
2022-06-28 09:44:49 -0400

    Initial Checkin

---
+++ grooving/DbProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grooving
+{
+    public class DbProvider
+    {
+        private string _dbPath;
+        public DbProvider(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public void Insert()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ grooving/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var isRunning = true;
+
+while (!isRunning)
+{
+    F
+}
