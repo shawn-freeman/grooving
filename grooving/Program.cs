ab8f55df9152dfb9681206582f90c9e61c2e06f7
Shawn Freeman <magus_zsf@hotmail.com>
2022-06-28 10:07:51 -0400

    Menu selection

--- grooving/Program.cs
+++ grooving/Program.cs
@@ -3,7 +3,32 @@ Console.WriteLine("Hello, World!");
 
 var isRunning = true;
 
-while (!isRunning)
+while (isRunning)
 {
-    F
+    
+    Console.WriteLine("1. Output Employee Records");
+    Console.WriteLine("2. Insert Employee Record");
+    Console.WriteLine("3. Update Employee Record");
+    Console.WriteLine("4. Delete Employee Record");
+    Console.Write("Enter Number Selection: ");
+
+
+    int menuSelection = 0;
+
+    while (IsValidMenuSelection(menuSelection))
+    {
+        var validSelection = Int32.TryParse(Console.ReadLine(), out menuSelection);
+
+        if (!validSelection || IsValidMenuSelection(menuSelection))
+        {
+            Console.Write("Enter Number Selection: ");
+        }
+    }
+
+    Console.WriteLine($"Selected {menuSelection}");
+}
+
+bool IsValidMenuSelection(int selection)
+{
+    return (selection < 1 || selection > 4);
 }
